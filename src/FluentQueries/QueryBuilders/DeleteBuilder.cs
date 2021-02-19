b9e570b8b577b32dee82c8edbf59b86f0095cf8b
MikhaZyu <MikhaZyu@gmail.com>
2021-02-19 23:17:29 +0300

    DeleteBuilder

---
+++ src/FluentQueries/QueryBuilders/DeleteBuilder.cs
@@ -0,0 +1,80 @@
+﻿using FluentQueries.Abstract;
+using FluentQueries.Models;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Text;
+
+namespace FluentQueries.QueryBuilders
+{
+    public class DeleteBuilder
+    {
+        private string _tableName = "";
+        private string _whereExpression = "";
+        private List<IQueryParameter> _parameters = new List<IQueryParameter>();
+
+        public DeleteBuilder Table(string tableName)
+        {
+            _tableName = tableName;
+
+            return this;
+        }
+
+        public DeleteBuilder Where(string whereExpression)
+        {
+            if (string.IsNullOrEmpty(_whereExpression))
+            {
+                _whereExpression = whereExpression;
+            }
+            else
+            {
+                _whereExpression += " AND " + whereExpression;
+            }
+
+            return this;
+        }
+
+        public DeleteBuilder Parameter(string name, object value)
+        {
+            var parameter = new QueryParameter(name, value);
+
+            _parameters.Add(parameter);
+
+            return this;
+        }
+
+        public DeleteBuilder Parameter(string name, object value, DbType dbType)
+        {
+            var parameter = new QueryParameter(name, value, dbType);
+
+            _parameters.Add(parameter);
+
+            return this;
+        }
+
+        public IQuery Query()
+        {
+            var query = new Query();
+            query.AddParameters(_parameters);
+
+            var sb = new StringBuilder();
+            sb.AppendLine($"DELETE [{_tableName}]");
+
+            if (!string.IsNullOrEmpty(_whereExpression))
+            {
+                sb.AppendLine($"WHERE {_whereExpression}");
+            }
+
+            query.Text = sb.ToString();
+
+            return query;
+
+        }
+
+        public static DeleteBuilder Make()
+        {
+            return new DeleteBuilder();
+        }
+
+    }
+}
---
+++ test/FluentQueries.UnitTests/DeleteBulderTests.cs
@@ -0,0 +1,30 @@
+﻿using FluentQueries.QueryBuilders;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace FluentQueries.UnitTests
+{
+    [TestFixture]
+    public class DeleteBulderTests
+    {
+        [Test]
+        public void Query_DeleteItemById()
+        {
+            var query = DeleteBuilder.Make()
+                .Table("Table")
+                .Where("Id = @id")
+                .Parameter("id", 1)
+                .Query();
+
+            var ethalon = @"DELETE [Table]
+WHERE Id = @id
+";
+            Assert.AreEqual(ethalon, query.Text);
+            Assert.AreEqual(1, query.Parameters.Count());
+
+        }
+    }
+}
